2b2ceb0ba69af96f6db8b2dbcc7144c264b499af
rramosx11 <rodolforamosd11@gmail.com>
2022-08-17 17:56:39 -0400

    added more to get route

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
-// const fs = require("fs");
-// const path = require("path");
+
 const { notes } = require("./db/db");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// const apiRoutes = require("./routes/apiRoutes");
-// const htmlRoutes = require("./routes/htmlRoutes");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
