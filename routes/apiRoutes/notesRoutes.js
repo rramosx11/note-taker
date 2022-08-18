76dcef6bcadbeb4b710e7ae368eb841cb43f2e2c
rramosx11 <rodolforamosd11@gmail.com>
2022-08-18 18:11:08 -0400

    added post route and its functions

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,15 +2,33 @@ const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const path = require("path");
-// const { createNewNote, deleteNote, findByID } = require
+// const { newNote, deleteNote, findByID } = require
 
-const notes = require("../../db/db.json");
+const { notes } = require("../../db/db.json");
 
 const NewNote = (note, notesArray) => {
   // adds new note to notes array
   notesArray.push(note);
 
   // saves notes array to db.json
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+};
+
+// finds specific note by ID from notes array
+const findById = (id, notesArray) => {
+  const result = notesArray.filter((note) => note.id === id)[0];
+  return result;
+};
+
+const deleteNote = (note, notesArray) => {
+  // removes specific note from notes array
+  const index = notesArray.indexOf(note);
+  notesArray.splice(index, 1);
+
+  // rewrites db.json with new array
   fs.writeFileSync(
     path.join(__dirname, "../db/db.json"),
     JSON.stringify({ notes: notesArray }, null, 2)
@@ -34,4 +52,9 @@ router.post("/notes", (req, res) => {
   res.json(req.body);
 });
 
+router.delete("/notes/:id", (req, res) => {
+  const note = findById(req.params.id, notes);
+  deleteNote(note, notes);
+  res.json;
+});
 module.exports = router;
